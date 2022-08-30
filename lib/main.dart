179d346e47d57b6bf329cc1d8a87bd327fb1c768
unknown <jumainahkhan@gmail.com>
2022-08-30 14:00:17 +0530

    new branch

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Jumainah is cuteeeee',
+      title: 'Jumainah\'s branch',
       theme: ThemeData(
         // This is the theme of your application.
         //
